e6ef282f9f1d15597e27608bbf3e15b680914dd9
mhnlz132012-svg <mhnlz132012@gmail.com>
2026-06-06 19:44:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+<<<<<<< HEAD
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    autoHideMenuBar: true
+  });
+
+  win.loadURL('https://www.2aclassweb.com');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+=======
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800
+  });
+
+  win.loadURL("https://www.2aclassweb.com");
+}
+
+app.whenReady().then(createWindow);
+>>>>>>> 84cc029014093022e03a1d5807942ede11276984
